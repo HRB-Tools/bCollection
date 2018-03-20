f565329040d38aaa1d46b498b6fcd94486f2293a
UKayeF <nilseidmann@gmail.com>
2018-03-20 16:01:08 +0100

    looks for beginning and end characters

---
+++ readInput.js
@@ -0,0 +1,14 @@
+function readInput(a) {
+  var hold1 = rawtext[a];
+  if ((hold1.lastIndexOf(".") == (hold1.length) - 1) && (hold1.length > 0)) { //Punkt vor Leerzeichen -> Satzende (Fehlerbeschreibung)
+    return 0;
+  }
+  if (hold1.indexOf(';;"') === 0) {
+    return 1;
+  }
+  
+  if (hold1.indexOf('!') >= 0) { //Testen ob ein Satz ein Ausrufezeichen enthält (Fehleranweisung)
+    return 2;
+  }
+   else return "-";
+}
